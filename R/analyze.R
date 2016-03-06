01acfa3c6bd7cab57e83939bd56c50e9542fc959
Chris C <chris.c.1221@gmail.com>
2016-03-06 17:55:27 -0500

    clean up al ittle

--- R/analyze.R
+++ R/analyze.R
@@ -6,21 +6,21 @@
 #' @param j Index 2
 #' @param k Index 3
 
-analyze <- function(i = double(), j = double(), k = double()){
+analyze <- function(i = double(), j = double()){
 
 	setwd(paste0("/scratch/hpc2862/CAMH/perm_container/container_",i,"_",j))
 
 	for(i in 1:10){
 		#clear out excess files
-		system(paste0("rm chr1_block_",i,"_perm_",k,"_k_",j,".cases.gen; ",
-			"rm chr1_block_",i,"_perm_",k,"_k_",j,".cases.haps;",
-			"rm chr1_block_",i,"_perm_",k,"_k_",j,".cases.sample;",
-			"rm chr1_block_",i,"_perm_",k,"_k_",j,".cases.summary;",
-			"rm chr1_block_",i,"_perm_",k,"_k_",j,".cases.legend;",
-			"rm chr1_block_",i,"_perm_",k,"_k_",j,".controls.sample;",
-			"rm chr1_block_",i,"_perm_",k,"_k_",j,".controls.haps;",
-			"rm chr1_block_",i,"_perm_",k,"_k_",j,".controls.summary;",
-			"rm chr1_block_",i,"_perm_",k,"_k_",j,".controls.legend"))}
+		system(paste0("rm chr1_block_",i,"_perm_k_",j,".cases.gen; ",
+			"rm chr1_block_",i,"_perm_k_",j,".cases.haps;",
+			"rm chr1_block_",i,"_perm_k_",j,".cases.sample;",
+			"rm chr1_block_",i,"_perm_k_",j,".cases.summary;",
+			"rm chr1_block_",i,"_perm_k_",j,".cases.legend;",
+			"rm chr1_block_",i,"_perm_k_",j,".controls.sample;",
+			"rm chr1_block_",i,"_perm_k_",j,".controls.haps;",
+			"rm chr1_block_",i,"_perm_k_",j,".controls.summary;",
+			"rm chr1_block_",i,"_perm_k_",j,".controls.legend"))}
 
 	########### this is the begining of rand.R ##################
 
