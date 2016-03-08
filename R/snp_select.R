1afcd87238814b6a3e90fafd3a589e326f6e7696
Chris C <chris.c.1221@gmail.com>
2016-03-07 20:23:34 -0500

    update snp select function

--- R/snp_select.R
+++ R/snp_select.R
@@ -13,7 +13,9 @@ snp.select <- function(summary = character()){
 	for(i in 1:max(summary$k)){
                 summary %>% filter(k==i) %>% sample_n(1000) %>% select(rsid, chromosome, position, all_maf, k) %>% rbind(snps, .) -> snps
         }
-
+	
+	colnames(snps) <- c("rsid", "chromosomes", "V3", "all_maf", "k")
+	
 	return(snps)
 	
 }
