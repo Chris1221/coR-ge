db22cac667d38cbcf1d13fbfbc76a6856b841b35
Chris C <chris.c.1221@gmail.com>
2016-05-24 11:08:47 +0100

    add new r2gen #19

--- R/r2gen2.R
+++ R/r2gen2.R
@@ -1,24 +1,46 @@
-gen[, -(1:5)] %>%
-  as.matrix %>%
-  t -> genM
+#' Takes gen file and calculates weighted allele scores (WAS)
+#'
+#' @param gen Truncated gen file with only causal snps
+#' @param snps Truncated summary file with only causal snps
+#'
+#' @import data.frame
+#' @import dplyr
+#'
+#' @return A vector of weighted allele scores
+#' @export
 
-# 9 betas
-b <- phen(.combR = genM)
+calculate_was <- function(gen = NULL, snps = NULL){
 
-for(i in seq(1, nrow(genM), by = 3)){
+  if(is.null(gen)) stop("Please input a gen matrix")
+  if(is.null(snps)) stop("Please input a snps matrix")
 
-  genM[i,] <- 0*genM[i,]
-  genM[i+1,] <- 1*genM[i+1,]
-  genM[i+2,] <- 2*genM[i+2,]
+  gen[, -(1:5)] %>%
+    as.matrix %>%
+    t -> genM
 
-}
+  # 9 betas
+  b <- phen(.combR = genM)
 
-p <- genM %*% b
+  for(i in seq(1, nrow(genM), by = 3)){
 
-p2 <- vector()
+    genM[i,] <- 0*genM[i,]
+    genM[i+1,] <- 1*genM[i+1,]
+    genM[i+2,] <- 2*genM[i+2,]
 
-for(i in seq(1, length(p), by = 3)){
+  }
 
-  p2[(i+2)/3] <- p[i]+p[i+1]+p[i+2]
+  p <- genM %*% b
+
+  maf <- snps$all_maf
+  p2 <- vector()
+
+  for(i in seq(1, length(p), by = 3)){
+
+    j <- (i+2)/3
+    p2[j] <- p[i]+p[i+1]+p[i+2]-maf[j] # this might not be perfect
+
+  }
+
+  return(p2)
 
 }
