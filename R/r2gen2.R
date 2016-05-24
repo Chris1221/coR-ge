45b6d16a48892139b7c49f0e2597fb76df3dec2d
Chris C <chris.c.1221@gmail.com>
2016-05-24 11:22:32 +0100

    update namespace

--- R/r2gen2.R
+++ R/r2gen2.R
@@ -3,7 +3,7 @@
 #' @param gen Truncated gen file with only causal snps
 #' @param snps Truncated summary file with only causal snps
 #'
-#' @import data.frame
+#' @import data.table
 #' @import dplyr
 #'
 #' @return A vector of weighted allele scores
