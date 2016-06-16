875d252be929d59a69c09d822f238424958081c5
Chris C <chris.c.1221@gmail.com>
2016-06-16 12:28:21 +0100

    update stratify to have pnc and pc explicitly as arguements

--- R/stratify.R
+++ R/stratify.R
@@ -12,20 +12,21 @@
 #'
 #' @export
 
-stratify <- function(snp_list = NULL, summary = NULL, p = NULL, n_strata = NULL){
+stratify <- function(snp_list = NULL, summary = NULL, pnc = NULL, pc = NULL, n_strata = NULL){
 
 	if(any(c(is.null(snp_list), is.null(summary), is.null(p), is.null(n_strata)))) stop("Missing a required input arguement")
 
-	# Only look at the ones not selected to be either real or fake
-	summary %>% filter(!(rsid %in% unlist(snp_list))) %>% mutate(h1 = F) ->
-		h0_summary
-	summary %>% filter(rsid %in% unlist(snp_list)) %>% mutate(h1 = T) ->
-		h1_summary
+	h0_summary <- summary %>% filter(!(rsid %in% unlist(snp_list))) %>% mutate(h1 = F)
+	h1_summary <- summary %>% filter(rsid %in% unlist(snp_list)) %>% mutate(h1 = T)
+	
+	h1_s1 <- h1_summary %>% sample_n(floor(nrow(h1_summary)*pc))
+	h1_s2 <- h1_summary %>% filter(!(rsid %in% h1_s1$rsid)) 
 
-	s1 <- h0_summary %>% sample_n(floor(nrow(h0_summary)*p) - length(snp_list))
-	s1 %<>% rbind(h1_summary) %>% mutate(s = 1)
+	s1 <- h0_summary %>% sample_n(floor(nrow(h0_summary)*pnc) - length(snp_list))
+	s2 <- h0_summary %>% filter(!(rsid %in% s1$rsid))
 
-	s2 <- summary %>% filter(!(rsid %in% s1$rsid)) %>% mutate(s = 2, h1 = F)
+	s1 %<>% rbind(h1_s1) %>% mutate(s = 1)
+	s2 %<>% rbind(h1_s2) %>% mutate(s = 2)
 
 	strata <- rbind(s1, s2)
 	return(strata)
