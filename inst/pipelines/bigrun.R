857d97a6b0dd88cef47b72fce1d49e5ef5ce51c3
Chris C <chris.c.1221@gmail.com>
2016-06-27 22:29:21 +0200

    fix typo

--- inst/pipelines/bigrun.R
+++ inst/pipelines/bigrun.R
@@ -71,7 +71,7 @@ foreach(i = c(1:10)) %:%
 	foreach(j = c(1:10)) %:%
 		foreach(h2 = seq(0.1, 0.9, by = 0.1)) %:%
 			foreach(pc = seq(0.1, 0.9, by = 0.1)) %:%
-				foreach(pnc = seq(0.1., 0.9, by = 0.1)) %:%
+				foreach(pnc = seq(0.1, 0.9, by = 0.1)) %:%
 					foreach(nc = seq(50,500, by =50)) %dopar% {
 						analyze(i = i, j = j, h2 = h2, pc = pc, pnc = pnc, nc = nc, local = TRUE, gen = gen, summary = summary)
 					}
