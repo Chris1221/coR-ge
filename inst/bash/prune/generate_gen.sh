6ac017fb5a364eb97d720c38c7a9e7fc266287d3
Christopher Cole <chris.c.1221@gmail.com>
2016-10-10 13:08:52 -0400

    error in options

--- inst/bash/prune/generate_gen.sh
+++ inst/bash/prune/generate_gen.sh
@@ -4,7 +4,7 @@ cd /home/hpc2862/Raw_Files/CAMH/1kg_hapmap_comb/hapmap3_r2_plus_1000g_jun2010_b3
 
 /home/hpc2862/Programs/hapgen2 \
        	-h pilot1.jun2010.b36.CEU.chr1.snpfilt.haps \
-	-l pilot1.jun2010.b36.CEU.chr1.snpfilt.legend 
+	-l pilot1.jun2010.b36.CEU.chr1.snpfilt.legend \
 	-m genetic_map_chr1_combined_b36.txt \
 	-dl 77053 1 1.0 1.0 \
        	-n 1000 0 \
