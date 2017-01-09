df1001ed65f98be08bea83db2113d194896db5f0
Chris1221 <chris.c.1221@gmail.com>
2017-01-09 20:50:10 +0000

    blah

--- inst/analyses/new_ref/run_coRge.R
+++ inst/analyses/new_ref/run_coRge.R
@@ -3,4 +3,4 @@ library(coRge)
 gen <- fread("/scratch/hpc2862/CAMH/new_corge/sim/chr1_sim1.controls.gen", h = F, sep = " ")
 summary <- fread("/scratch/hpc2862/CAMH/new_corge/summary", h = T, sep = " ")
 
-analyze(gen, summary, local = T)
+analyze(gen, summary, local = T, output ="/scratch/hpc2862/CAMH/new_corge/results/run1.txt" )
