95557295d5d96af20f4323c0089ea1402cf946e2
Chris1221 <chris.c.1221@gmail.com>
2017-01-09 20:47:38 +0000

    add new run file

---
+++ inst/analyses/new_ref/run_coRge.R
@@ -0,0 +1,6 @@
+library(coRge)
+
+gen <- fread("/scratch/hpc2862/CAMH/new_corge/sim/chr1_sim1.controls.gen", h = F, sep = " ")
+summary <- fread("/scratch/hpc2862/CAMH/new_corge/summary", h = T, sep = " ")
+
+analyze(gen, summary, local = T)
