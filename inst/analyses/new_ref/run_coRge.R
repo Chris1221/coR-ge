d609ac3be68abfabfa8cf3da779e16bbbe902de2
Chris1221 <chris.c.1221@gmail.com>
2017-01-19 09:36:44 +0000

    update sub

--- inst/analyses/new_ref/run_coRge.R
+++ inst/analyses/new_ref/run_coRge.R
@@ -1,7 +1,19 @@
 library(coRge)
 library(data.table)
 
-gen <- fread("/scratch/hpc2862/CAMH/new_corge/sim/chr1_sim1.controls.gen", h = F, sep = " ", data.table = F, nrow = 100000)
-summary <- fread("/scratch/hpc2862/CAMH/new_corge/summary", h = T, sep = " ")
+gen <- fread("/scratch/hpc2862/CAMH/new_corge/sim/chr1_sim1.controls.gen",
+	     h = F,
+	     sep = " ",
+	     data.table = F)
 
-analyze(gen, summary, local = T, output ="/scratch/hpc2862/CAMH/new_corge/results/run2.txt" )
+summary <- fread("/scratch/hpc2862/CAMH/new_corge/summary/stats.txt",
+		 h = T,
+		 sep = " ")
+
+analyze(i=1,
+	j = 1,
+	nc = 1000,
+	gen = gen,
+	summary = summary,
+	local = T,
+	output ="/scratch/hpc2862/CAMH/new_corge/results/run2.txt")
